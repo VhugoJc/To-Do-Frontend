fdbae1c432b3dc9de2e3001c01166c8ef15696d8
VhugoJC <vic_360.2000@hotmail.com>
2023-05-21 15:17:18 -0600

    ⚡️ Add context in Filter form

--- src/shared/FilterControl/index.jsx
+++ src/shared/FilterControl/index.jsx
@@ -2,41 +2,67 @@ import { Button, Card, Col, Form, Input, Row, Select } from "antd";
 import { SearchOutlined } from '@ant-design/icons';
 import './FilterControl.scss';
 import useFilter from "../../Hooks/useFilter";
+import { useEffect } from "react";
 
 const FilterControl = () => {
     const {filterData, setFilterData} = useFilter();
+    const [form] = Form.useForm();
     
+    useEffect(()=>{
+        form.setFieldsValue(filterData);
+    },[]);
+
+    const onFinish = (value) => {
+        setFilterData(value);
+    }
+
     return (
         <div>
             <Row>
                 <Col md={1} />
                 <Col xs={24} md={22}>
                     <Card className="card-filter">
-                        <Form>
+                        <Form
+                        form={form}
+                        onFinish={onFinish}
+                        >
                             <Row gutter={8}>
                                 <Col className="card-filter-col" md={12} xs={24}>
-                                    <p className="card-filter-label">Name</p>
-                                    <Input prefix={<SearchOutlined />} placeholder="What are you looking for?" />
+                                    <Form.Item
+                                        label="Name"
+                                        name="name"
+                                    >
+                                        <Input prefix={<SearchOutlined />} placeholder="What are you looking for?" />
+                                    </Form.Item>
                                 </Col>
                                 <Col md={4} xs={12} className="card-filter-col">
-                                    <p className="card-filter-label">Priority</p>
+                                    <Form.Item
+                                        label="Priority"
+                                        name="priority"
+                                    >
                                     <Select
                                         className="card-filter-select"
-                                        defaultValue={'Low'}
-                                        options={[{ value: 'Low', label: 'Low' }, { value: 'High', label: 'High' }, { value: 'All', label: 'All' }]}
+                                        options={[{ value: 'low', label: 'Low' }, { value: 'high', label: 'High' }, { value: 'all', label: 'All' }]}
                                     />
+                                    </Form.Item>
                                 </Col>
                                 <Col md={4} xs={12} className="card-filter-col">
-                                    <p className="card-filter-label">State</p>
-                                    <Select
-                                        className="card-filter-select"
-                                        defaultValue={'Done'}
-                                        options={[{ value: 'Done', label: 'Done' }, { value: 'Undone', label: 'Undone' },{value:'All',label:'All'}]}
-                                    />
+                                    <Form.Item
+                                        label="Status"
+                                        name="status"
+                                    >
+                                        <Select
+                                            className="card-filter-select"
+                                            options={[{ value: 'done', label: 'Done' }, { value: 'undone', label: 'Undone' },{value:'all',label:'All'}]}
+                                            />
+                                        </Form.Item>
                                 </Col>
                                 <Col md={4} xs={24} className="card-filter-col">
-                                    <p className="card-filter-hidden">Search</p>
-                                    <Button className="card-filter-btn">Search</Button>
+                                    <Form.Item
+                                        label=" "
+                                    >
+                                        <Button htmlType="submit" className="card-filter-btn">Search</Button>
+                                    </Form.Item>
                                 </Col>
                             </Row>
                         </Form>
