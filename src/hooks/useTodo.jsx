ac52eb9965b7c9f5a67d975a10837c2425d841c2
VhugoJC <vic_360.2000@hotmail.com>
2023-05-21 14:43:27 -0600

    🐛 Fix Buf

--- src/hooks/useTodo.jsx
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import ToDoContext from "../context/TodoProvider";
-
-const useToDo = () => {
-    return useContext(ToDoContext);
-}
-
-export default useToDo;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './scss/index.scss';
 import reportWebVitals from './reportWebVitals';
 import Home from './pages/Home';
-import { ToDoProvider } from './context/TodoProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ToDoProvider>  {/* useContext */}
-    <React.StrictMode>
-      <Home />
-    </React.StrictMode>
-  </ToDoProvider>
+  <React.StrictMode>
+    <Home/>
+  </React.StrictMode>
 );
 reportWebVitals();
