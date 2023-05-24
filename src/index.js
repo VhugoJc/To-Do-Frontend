53a7aa395a545deb2448eac99286734972333f47
VhugoJC <vic_360.2000@hotmail.com>
2023-05-24 11:08:30 -0600

    ✨ Add Console ascii logo

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import reportWebVitals from './reportWebVitals';
 import Home from './pages/Home';
 import { ToDoProvider } from './Context/TodoProvider';
 import { ModalProvider } from './Context/ModalProvder';
+import { printAsciiLogo } from './config/ascii_logo';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+printAsciiLogo();
 root.render(
   <ModalProvider>
     <ToDoProvider>
