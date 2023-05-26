4cbbaa3a12e48177af53b8370e50750550b8da8c
VhugoJC <vic_360.2000@hotmail.com>
2023-05-26 13:08:01 -0600

    🔥 Remove unecessary files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './scss/index.scss';
-import reportWebVitals from './reportWebVitals';
 import Home from './pages/Home';
+
 import { ToDoProvider } from './Context/TodoProvider';
 import { ModalProvider } from './Context/ModalProvder';
 import { printAsciiLogo } from './config/ascii_logo';
@@ -22,4 +22,3 @@ root.render(
     <Home />
   </Providers>
 );
-reportWebVitals();
